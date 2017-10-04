cbc3ef92ba9aef16f04e872780b342b896551c0c
Devin Blitzer <dblitz@Devins-MacBook-Pro.local>
2017-10-04 00:23:05 -0500

    Amazon Review output to CSV

--- amazoncrawler.py
+++ amazoncrawler.py
@@ -1,22 +1,18 @@
 import urllib2
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
+import csv
+import pandas as pd
+
 
 
 ua = UserAgent()
+headers = {'User-Agent':str(ua.chrome)}
 
-# try:
-#    urllib2.urlopen("some url")
-# except urllib2.HTTPError as err:
-#    if err.code == 404:
-#        <whatever>
-#    else:
-#        raise
-
-# headers = {'User-agent': 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36'}
-# headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 def getReview(pageNumber):
-    pageNumber = pageNumber + 1;
+
+    amazonReviewArray = [];
+    pageNumber;
     page = urllib2.urlopen("https://www.amazon.com/Converse-Unisex-Chuck-Taylor-Sneakers/product-reviews/B01G2N1WKU/ref=cm_cr_arp_d_paging_btm_2?ie=UTF8&reviewerType=avp_only_reviews&pageNumber=" + str(pageNumber))
 
     # page = urllib2.urlopen("https://www.amazon.com/adidas-CQ1190-Womens-Originals-Superstar/product-reviews/B01N2W6F8A/ref=cm_cr_getr_d_paging_btm_4?ie=UTF8&reviewerType=avp_only_reviews&pageNumber=" + str(pageNumber))
@@ -27,41 +23,40 @@ def getReview(pageNumber):
     for reviewNumber in range(10):
 
         theReview = beforeReview.split('</span>',1)[0]
-        print("\n\n" + theReview);
+        # print("\n\n" + theReview);
+        amazonReviewArray.append(theReview);
 
         if(reviewNumber < 9):
             beforeReview = beforeReview.split('review-body">',1)[1];
         else:
             break;
+    # print("This is the array : Amazon Review Array " + str(number) + "   " + str(amazonReviewArray))
+
+    # print(amazonReviewArray);
+    return amazonReviewArray;
 
 
-headers = {'User-Agent':str(ua.chrome)}
+
+#return list of reviews from the page
+#then append that list to all reviews
+#then return ALL reviews
 
 
 def getAllReviews(howManyPages):
 
+    allAmazonReviewsArray = []
     theCurrentPageNumber = 1;
-
-    #uf TCPN is not 10 that means that it was 
-    #not the original 10 input and that it 
-    #was called from a 503 error
-    # if(TCPN == 10):
-    #     return;
-    # if(TCPN == 11):
-    #     theCurrentPageNumber = 1;
-    #     theRange = TCPN - 1
-    # else:
-    #     theCurrentPageNumber = TCPN;
-    #     theRange = 10 - TCPN;
-
     for pageNumber in range(howManyPages):
 
         try:
-           getReview(theCurrentPageNumber)
+           theReviewsFromPage = getReview(theCurrentPageNumber)
+           # print(theReviewsFromPage)
+           allAmazonReviewsArray.extend(theReviewsFromPage);
+           # print(allAmazonReviewsArray)
            theCurrentPageNumber = theCurrentPageNumber + 1;
-           print("The current page number is " + str(theCurrentPageNumber));
-           if(theCurrentPageNumber == howManyPages):
-                return;
+           print("Getting page ## " + str(theCurrentPageNumber-1));
+           if(theCurrentPageNumber == howManyPages + 1):
+                return allAmazonReviewsArray;
 
         except urllib2.HTTPError as err:
            if err.code == 503:
@@ -69,8 +64,46 @@ def getAllReviews(howManyPages):
                 # getAllReviews(theCurrentPageNumber);
                 # if(theCurrentPageNumber == 10):
                 #     return;
+    # print("It is none" + allAmazonReviewsArray);
 
-getAllReviews(20);
+
+# theReview = review;
+# amazonReviewArray = theReview;
+# # print(text);
+
+# def setupColumns():
+#   with open('reviews.csv', 'w') as outfile:
+#     mywriter = csv.writer(outfile)
+#     mywriter.writerow(['review'])
+
+
+# setupColumns();
+
+# #save comments to CSV file
+theReviewsOutput = getAllReviews(20)
+
+df = pd.DataFrame(theReviewsOutput, columns=["review"])
+df.to_csv('reviews.csv', index=False)
+
+# with open('reviews.csv', 'wb') as myfile:
+#     wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+#     wr.writerow(theReviewsOutput)
+
+# theFile = open('reviews.csv', 'w')
+# wr = csv.writer(theFile, quoting=csv.QUOTE_ALL)
+# wr.writerow(theReviewsOutput)
+
+# for theReviews in theReviewsOutput:
+
+#     theFile = open('reviews.csv', 'a')
+#     mywriter = csv.writer(theFile)
+
+#     print(theReviews)
+#     mywriter.writerow(theReviews)
+
+
+#     print("Done")
+# print("Why is it noneeeee" + str(getAllReviews(5)));
 
 
 
